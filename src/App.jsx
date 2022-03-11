2299f234c30f7f8be07c584b4beecccb4ea8f57c
MatheusKeidy <matheuskeidygomes@gmail.com>
2022-03-11 05:58:22 -0300

    react router att

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from "./components/layout/index";
 import Home from './pages/home/index';
+import Games from './pages/games/index';
+import Wallpapers from './pages/wallpapers/index';
 import Header from './components/header';
 import Footer from "./components/footer";
 
@@ -12,7 +15,17 @@ export default function App() {
 
       <Header />
 
-      <Home />
+      <BrowserRouter>
+
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/wallpaper" element={<Wallpapers />} />
+
+        </Routes>
+
+      </BrowserRouter>
 
     </Layout>
 
--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -13,9 +13,9 @@ export default function Menu(props) {
 
                 <img className="w-10 md:hidden absolute top-5 right-5 cursor-pointer" src={quit} onClick={() => props.onClick()} alt="quit" />
 
-                <li className="font p-3 lg:p-5 mt-16 md:mt-0"> <a href="/"> HOME </a> </li>
-                <li className="font p-3 lg:p-5"> GAMES </li>
-                <li className="font p-3 lg:p-5"> WALLPAPERS </li>
+                <Link to="/"> <li className="font p-3 lg:p-5 mt-16 md:mt-0"> HOME </li> </Link>
+                <Link to="/games"> <li className="font p-3 lg:p-5"> GAMES </li> </Link>
+                <Link to="/wallpapers"> <li className="font p-3 lg:p-5"> WALLPAPERS </li> </Link>
 
                 <div className="mt-10 md:hidden">
                     <Social menuActive={props.menuActive} />
