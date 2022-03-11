85bd141b36b51075411ec0836e705ccbfb141f48
MatheusKeidy <matheuskeidygomes@gmail.com>
2022-03-11 06:58:07 -0300

    game banner att 3

--- src/pages/games/index.jsx
+++ src/pages/games/index.jsx
@@ -5,7 +5,7 @@ export default function Games() {
 
     return <>
 
-        <div class="border-t-2 border-b-2 border-gray-900 flex justify-center items-center bg-cover bg-no-repeat bg-center " style={{ backgroundImage: `url(${Exclusives})`, height:400 }}>
+        <div class="border-t-2 border-b-2 border-gray-900 flex justify-center items-center bg-cover bg-no-repeat bg-center " style={{ backgroundImage: `url(${Exclusives})`, height:540 }}>
 
             <div class="bg-black rounded-md p-5 opacity-70">
                     <span className="opacity-100"> olá </span> 
